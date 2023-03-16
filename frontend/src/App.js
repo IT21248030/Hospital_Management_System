dc84ec67088d1663fa9eda8d191facd7999e85a4
Kula <99582220+it21279898@users.noreply.github.com>
2023-03-16 23:20:46 +0530

    create inventorycontroller and inventorymdel files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      buwani
-=======
-      Nikini 
->>>>>>> dcff1fb2588766c275995e2779b16ba23244b525
+
+
+
+
+
+
+
     </div>
   );
 }
